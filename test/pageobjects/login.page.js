824f386190c85cb5e8f63f980971657f80e984fe
atiarmridul <atiarmridul@gmail.com>
2024-08-27 16:20:10 +0600

    Login implemented.

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,15 +1,11 @@
 import { $ } from '@wdio/globals'
 import Page from './page.js';
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
+
 class LoginPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
+
     get inputUsername () {
-        return $('#username');
+        return $('#email');
     }
 
     get inputPassword () {
@@ -20,19 +16,17 @@ class LoginPage extends Page {
         return $('button[type="submit"]');
     }
 
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
+
     async login (username, password) {
         await this.inputUsername.setValue(username);
         await this.inputPassword.setValue(password);
         await this.btnSubmit.click();
+        await browser.pause(5000);
+       
+
     }
 
-    /**
-     * overwrite specific options to adapt it to page object
-     */
+
     open () {
         return super.open('login');
     }
