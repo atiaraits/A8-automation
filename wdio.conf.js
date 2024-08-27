b015d2dccf4cca156c026f92c36c4c461bf1b332
atiarmridul <atiarmridul@gmail.com>
2024-08-27 14:55:20 +0600

    project inti

---
+++ wdio.conf.js
@@ -0,0 +1,46 @@
+export const config = {
+
+    runner: 'local',
+
+    specs: [
+        './test/specs/**/*.js'
+    ],
+ 
+    exclude: [
+        // 'path/to/excluded/files'
+    ],
+
+    before: function (capabilities, specs) {
+        browser.maximizeWindow();
+    },
+
+    logLevel: 'error',
+    bail: 0,
+    capabilities: [
+        {
+            maxInstances: 10,
+            browserName: 'chrome',
+            acceptInsecureCerts: true
+        },
+        // {
+        //     maxInstances: 10,
+        //     browserName: 'firefox',
+        //     acceptInsecureCerts: true
+        // }
+    ],
+
+
+    waitforTimeout: 50000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    services: [],
+
+    framework: 'mocha',
+
+    reporters: ['spec'],
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+    baseUrl: 'http://localhost'
+}
